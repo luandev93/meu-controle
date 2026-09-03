71e43297cd439118eaf4f9e154a1e5ff146ee248
luandev93 <luandev93@gmail.com>
2026-09-03 15:03:07 -0300

    chore: invalidate PWA cache after persistence fixes

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'controle-v2';
+const CACHE_NAME = 'controle-v3';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
